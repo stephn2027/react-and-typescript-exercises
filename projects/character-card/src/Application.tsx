68e699ccc886cac065049cfb57049e0762b22dea
stephen <stephn2027@gmail.com>
2023-04-13 13:38:04 +0900

    finished character card exer

--- projects/character-card/src/Application.tsx
+++ projects/character-card/src/Application.tsx
@@ -6,7 +6,21 @@ import { Loading } from './Loading';
 import { CharacterInformation } from './CharacterInformation';
 
 const Application = () => {
-  return <main></main>;
+  const [character, setCharacter] = React.useState<CharacterType | null>(null);
+  const [isLoading, setLoading] = React.useState<boolean>(true);
+  React.useEffect(() => {
+    setTimeout(() => {
+      fetchCharacter().then((c) => setCharacter(c));
+      setLoading(!isLoading);
+    }, 2000);
+  }, []);
+
+  return (
+    <main>
+      {isLoading && <Loading />}
+      {character && <CharacterInformation character={character} />}
+    </main>
+  );
 };
 
 export default Application;

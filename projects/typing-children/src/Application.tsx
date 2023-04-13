73b5247dabd65d1bfdef38fa85ce72f9ea4836f1
stephen <stephn2027@gmail.com>
2023-04-13 12:09:40 +0900

    typing children types done

--- projects/typing-children/src/Application.tsx
+++ projects/typing-children/src/Application.tsx
@@ -5,14 +5,15 @@
  * JSX.Element | JSX.Element[];
  * React.ReactNode;
  * React.ReactChildren;
- * React.ReactChild[];
+ * React.ReactChild[];;
  */
+import * as React from 'react';
 
- type BoxProps = { children: any };
+ type BoxProps = { children: React.ReactNode,style?:React.CSSProperties};
 
- const Box = ({ children }: BoxProps) => {
+ const Box = ({ children,style={} }: BoxProps) => {
    return (
-     <section style={{ padding: "1em", border: "5px solid purple" }}>
+     <section style={{ padding: "1em", border: "5px solid purple",...style }}>
        {children}
      </section>
    );
@@ -23,7 +24,7 @@
      <Box>
        Just a string.
        <p>Some HTML that is not nested.</p>
-       <Box>
+       <Box style={{borderColor:'red'}}>
          <h2>Another React component with one child.</h2>
        </Box>
        <Box>

35f72c4ceebd149077df8bbc8a40174b4f3c7b47
stephen <stephn2027@gmail.com>
2023-04-14 11:51:36 +0900

    finished counter exercise for classes typescript

--- projects/counter-for-hooks/src/Application.tsx
+++ projects/counter-for-hooks/src/Application.tsx
@@ -1,24 +1,58 @@
-const Counter = () => {
-  return (
-    <main className="Counter">
-      <h1>Days Since Last Incident</h1>
-      <p className="count">0</p>
-      <section className="controls">
-        <button>Increment</button>
-        <button>Reset</button>
-        <button>Decrement</button>
-      </section>
-      <section className="controls">
-        <form onSubmit={() => {}}>
-          <label htmlFor="set-to">Set Count</label>
-          <input id="set-to" type="number" />
-          <input type="submit" />
-        </form>
-      </section>
-    </main>
-  );
+import { Component } from 'react';
+
+type CounterProps = {
+  incident: string;
+};
+type CounterState = {
+  count: number;
 };
 
-const Application = () => <Counter />;
+class Counter extends Component<CounterProps, CounterState> {
+  state: CounterState = {
+    count: 0
+  };
+  increment = () => {
+    this.setState(({ count }) => ({ count: count + 1 }));
+  };
+  decrement = () => {
+    this.setState(({ count }) => ({ count: count - 1 }));
+  };
+  reset = () => {
+    this.setState(({ count }) => ({ count: 0 }));
+  };
+  changeCount = (event:React.ChangeEvent<HTMLInputElement>)=>{
+    this.setState({count:+event.target.value})
+  }
+
+  render() {
+    const { incident } = this.props;
+    const { count } = this.state;
+    return (
+      <main className="Counter">
+        <h1>Days Since Last {incident}</h1>
+        <p className="count">{count}</p>
+        <section className="controls">
+          <button onClick={()=>this.increment()}>Increment</button>
+          <button onClick={()=>this.reset()}>Reset</button>
+          <button onClick={()=>this.decrement()}>Decrement</button>
+        </section>
+        <section className="controls">
+          <form onSubmit={() => {}}>
+            <label htmlFor="set-to">Set Count</label>
+            <input id="set-to" type="number" onChange={this.changeCount} value={count} />
+            <input type="submit" />
+          </form>
+        </section>
+      </main>
+    );
+  }
+}
+
+class Application extends Component {
+  render() {
+    return <Counter incident="coffee spill" />;
+  }
+}
 
 export default Application;
+

dfb243732a69e024af0527ccb519a1f3791626e6
stephen <stephn2027@gmail.com>
2023-04-27 20:42:19 +0900

    added reducer counter exercise

---
+++ projects/counter-for-hooks/src/ApplicationCounterReducer.tsx
@@ -0,0 +1,48 @@
+import { useReducer } from "react";
+
+type CounterState ={
+  value:number
+}
+type BasicCounterACtion = {
+  type: 'decrement'|'increment'
+}
+
+type SetCounterAction = {
+  type: 'set';
+  payload: number;
+}
+
+type CounterAction = SetCounterAction | BasicCounterACtion;
+
+const reducer = (state:CounterState,action:CounterAction)=>{
+  switch(action.type){
+    case 'increment':
+      return {value:state.value + 1}
+    case 'decrement':
+      return {value:state.value - 1}
+    case 'set':
+      return {value:action.payload}
+    default: 
+      return state;
+  }
+}; 
+ 
+
+const Counter = () => {
+  const [state,dispatch] = useReducer(reducer,{value:0})
+  return (
+    <main className="Counter">
+      <h1>Days Since Last Incident</h1>
+  <p className="count">{state.value}</p>
+      <section className="controls">
+        <button onClick={()=>dispatch({type:'increment'})}>Increment</button>
+        <button onClick={()=>dispatch({type:'set',payload:0})}>Reset</button>
+        <button onClick={()=>dispatch({type:'decrement'})}>Decrement</button>
+      </section>
+    </main>
+  );
+};
+
+const Application = () => <Counter />;
+
+export default Application;

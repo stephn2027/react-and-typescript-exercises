4665e173e5ece5e60824fd290da1b10fc3d3ec2e
stephen <stephn2027@gmail.com>
2023-05-11 11:54:55 +0900

    ts color slider exercise (passing props using context api)

--- projects/color-swatch-base/src/Application.tsx
+++ projects/color-swatch-base/src/Application.tsx
@@ -5,22 +5,46 @@ import { ColorInputs } from './ColorInputs';
 import { ColorSliders } from './ColorSliders';
 
 import { toRGB } from './utilities';
-import { reducer } from './reducer';
+
+
+export interface RGBColorType  {
+  red: number;
+  green:number;
+  blue:number;
+}
+
+export type AdjustmentAction ={
+  type: 'ADJUST_RED'|'ADJUST_GREEN'|'ADJUST_BLUE';
+  payload:number;
+}
+
+const reducer = (state:RGBColorType,action:AdjustmentAction)=>{
+   switch(action.type){
+    case 'ADJUST_RED':
+      return {...state,red:action.payload}
+    case 'ADJUST_GREEN':
+      return {...state,green:action.payload}
+    case 'ADJUST_BLUE':
+      return {...state,blue:action.payload}
+    default : 
+      return state
+   }
+}
+
+const colorChoices:RGBColorType = {red:0,green:0,blue:0};
 
 const Application = () => {
-  const [rgb, dispatch] = React.useReducer(reducer, {
-    red: 0,
-    green: 0,
-    blue: 0
-  });
+  const [rgb, dispatch] = React.useReducer(reducer, colorChoices);
+
 
   return (
     <main style={{ borderColor: toRGB(rgb) }}>
       <ColorSwatch {...rgb} />
       <ColorInputs {...rgb} />
-      <ColorSliders {...rgb} />
+      <ColorSliders {...rgb} dispatch={dispatch} />
     </main>
   );
 };
 
 export default Application;
+
--- projects/color-swatch-base/src/ColorSlider.tsx
+++ projects/color-swatch-base/src/ColorSlider.tsx
@@ -1,14 +1,36 @@
-export interface ColorInputProps {
+import { ChangeEvent, Dispatch } from "react";
+import { AdjustmentAction, RGBColorType } from "./Application";
+
+
+export interface ColorInputProps{
   id: string;
   label: string;
   value: number;
+  onChange:(event:ChangeEvent<HTMLInputElement>)=>void;
 }
 
-export const ColorSlider = ({ id, label, value }: ColorInputProps) => {
+interface colorSiderProps extends RGBColorType {
+  dispatch:Dispatch<AdjustmentAction>;
+}
+
+const adjustRed= (event: ChangeEvent<HTMLInputElement>) => {
+  dispatch({ type: "ADJUST_RED", payload: +event.target.value });
+};
+
+const adjustGreen = (event: ChangeEvent<HTMLInputElement>) => {
+  dispatch({ type: "ADJUST_GREEN", payload: +event.target.value });
+};
+
+const adjustBlue = (event: ChangeEvent<HTMLInputElement>) => {
+  dispatch({ type: "ADJUST_BLUE", payload: +event.target.value });
+};
+
+
+export const ColorSlider = ({ id, label, value, onChange }: ColorInputProps) => {
   return (
     <div className="color-slider">
       <label htmlFor={id}>{label}</label>
-      <input id={id} type="range" min="0" max="255" value={value} />
+      <input id={id} type="range" min="0" max="255" value={value} onChange={onChange} />
     </div>
   );
 };
